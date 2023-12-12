72eec36bc469755d71ada5b273e917b7a5340907
azzamaddouri <maddouriazza@gmail.com>
2023-12-12 19:44:04 +0100

    Upload to firebase storage from gallery

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -8,11 +8,13 @@ import { AppComponent } from './app.component';
 import { AppRoutingModule } from './app-routing.module';
 import { initializeApp, provideFirebaseApp } from '@angular/fire/app';
 import { getStorage, provideStorage } from '@angular/fire/storage';
+import { getFirestore, provideFirestore } from '@angular/fire/firestore';
 
 @NgModule({
   declarations: [AppComponent],
   imports: [BrowserModule, IonicModule.forRoot(), AppRoutingModule, provideFirebaseApp(() => 
-    initializeApp({ "projectId": "fir-media-hub", "appId": "1:357292092788:web:7a0fae8e9da0a393b85021", "storageBucket": "fir-media-hub.appspot.com", "apiKey": "AIzaSyBcXAdTAYVRXJIFDkklAthihXwVTcB9Rxs", "authDomain": "fir-media-hub.firebaseapp.com", "messagingSenderId": "357292092788", "measurementId": "G-HM1EEV8QRB" })), provideStorage(() => getStorage())],
+    initializeApp({ "projectId": "fir-media-hub", "appId": "1:357292092788:web:7a0fae8e9da0a393b85021", "storageBucket": "fir-media-hub.appspot.com", "apiKey": "AIzaSyBcXAdTAYVRXJIFDkklAthihXwVTcB9Rxs", "authDomain": "fir-media-hub.firebaseapp.com", "messagingSenderId": "357292092788", "measurementId": "G-HM1EEV8QRB" })), 
+    provideStorage(() => getStorage()), provideFirebaseApp(() => initializeApp({"projectId":"fir-media-hub","appId":"1:357292092788:web:7a0fae8e9da0a393b85021","storageBucket":"fir-media-hub.appspot.com","apiKey":"AIzaSyBcXAdTAYVRXJIFDkklAthihXwVTcB9Rxs","authDomain":"fir-media-hub.firebaseapp.com","messagingSenderId":"357292092788","measurementId":"G-HM1EEV8QRB"})), provideFirestore(() => getFirestore())],
   providers: [{ provide: RouteReuseStrategy, useClass: IonicRouteStrategy }],
   bootstrap: [AppComponent],
 })
